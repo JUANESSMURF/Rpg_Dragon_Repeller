5c87b6b7c0cd85fc92701fc85dbca4331351cfa1
Juan <je.ortiz10003@gmail.com>
2024-01-27 15:26:59 -0500

    Add the locations object win and the function winthegame

--- script.js
+++ script.js
@@ -58,6 +58,12 @@ const locations = [
     "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
     "button functions": [restart, restart, restart],
     text: 'You die. ☠️'
+  },
+  {
+    name: "win",
+    "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
+    "button functions": [restart, restart, restart],
+    text: "You defeat the dragon! YOU WIN THE GAME! 🎉"
   }
 ];
 const monsters = [
@@ -183,7 +189,7 @@ function attack() {
   if (health <= 0) {
     lose();
   } else if (monsterHealth <= 0) {
-    defeatMonster();
+      fighting === 2 ? winGame() : defeatMonster();
   }
 
 }
@@ -203,6 +209,10 @@ function defeatMonster() {
 
 function lose() {
   update(locations[5]);
+}
+
+function winGame() {
+  update(locations[6]);
 }
 
 function restart() {
